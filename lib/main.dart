ed330081381601283963ab6f76382e862d749e01
Rachael Yoder <wayoder4@gmail.com>
2021-09-30 16:26:15 -0400

    initial setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/cupertino.dart';
-import 'package:recipe_book_app/app.dart';
 
 void main() {
   runApp(CookBookApp());
+}
+
+class CookBookApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      theme: const CupertinoThemeData(brightness: Brightness.light),
+      home: Container(),
+    );
+  }
 }
