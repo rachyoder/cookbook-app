1c6f6e9e73fa51366fba83420f684b37920ca9d7
Rachael Yoder <wayoder4@gmail.com>
2021-09-29 22:48:53 -0400

    initial commit

---
+++ android/app/src/main/kotlin/amishapplications/recipe_book_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package amishapplications.recipe_book_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+import 'package:recipe_book_app/app.dart';
+
+void main() {
+  runApp(CookBookApp());
+}
